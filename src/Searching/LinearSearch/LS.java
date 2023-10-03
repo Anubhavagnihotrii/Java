ca14eec9e268332144479da729705bcd97eb2467
Anubhav <anubahvagnihotri@gmail.com>
2023-10-03 18:41:51 +0530

    Added the code for Linear Search.
    Added the code for Linear Search.

---
+++ src/Searching/LinearSearch/LS.java
@@ -0,0 +1,24 @@
+package Searching.LinearSearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {10,20,30,40,50,50,60,70,80};
+        System.out.println("Array elements are :- "+ Arrays.toString(arr));
+        LinearSearch ls1 = new LinearSearch();
+        int x = ls1.linearSearch(arr,50);
+
+        if(x==-1)
+        {
+            System.out.println("Element not found/ not present in the array");
+        }
+        else
+        {
+            System.out.println("Element is at position "+x);
+        }
+        sc.close();
+    }
+}
