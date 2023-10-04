15b8f85bf5963adfe762bc0df9ba0079cb3936e5
Anubhav <anubahvagnihotri@gmail.com>
2023-10-05 00:54:22 +0530

    Added the HashSet code.

--- src/CollectionsFramework/Set/HashSet/hashSetsInCollections.java
+++ src/CollectionsFramework/Set/HashSet/hashSetsInCollections.java
@@ -1,7 +1,28 @@
 package CollectionsFramework.Set.HashSet;
 
+import java.util.HashSet;
+
+//n Java, a HashSet is a part of the Java Collections Framework and is one of the implementations of the Set interface. It represents a collection of unique elements, meaning that it cannot contain duplicate values.
+
 public class hashSetsInCollections {
     public static void main(String[] args) {
+        // Create a HashSet of strings
+        HashSet<String> fruits = new HashSet<>();
 
+        // Add elements to the HashSet
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        fruits.add("Apple"); // Duplicate, won't be added
+
+        // Check if an element exists in the HashSet
+        boolean containsBanana = fruits.contains("Banana");
+        System.out.println("Contains Banana: " + containsBanana);
+
+        // Iterate through the elements
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+//      The order of elements when iterating through the HashSet may vary.
     }
 }
--- src/RemoveDuplicatesArray/removeDuplicates.java
+++ src/RemoveDuplicatesArray/removeDuplicates.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class removeDuplicates {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,3,3,4,5,5,5,6,6,7,8};
-        int brr[] = new int[arr.length];
+        int[] arr = {1,2,3,3,3,4,5,5,5,6,6,7,8};
+        int[] brr = new int[arr.length];
 
         brr[0]=arr[0];
         int j=0;
