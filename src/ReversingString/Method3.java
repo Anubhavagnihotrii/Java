6609312b98b73fba0f89911959e93fbcc32d7b0a
Anubhav <anubahvagnihotri@gmail.com>
2023-10-08 22:00:13 +0530

    Reversing String Code Added.

---
+++ src/ReversingString/Method3.java
@@ -0,0 +1,18 @@
+package ReversingString;
+
+public class Method3 {
+    public static void main(String[] args) {
+        // Input string to be reversed
+        String s1 = "Anubhav is Awesome";
+
+        // Create a StringBuilder object and initialize it with the input string
+        StringBuilder s2 = new StringBuilder(s1);
+
+        // Print the original string
+        System.out.println("Original String: " + s2);
+
+        // Reverse the StringBuilder and print the reversed string
+        StringBuilder reversedString = s2.reverse();
+        System.out.println("Reversed String: " + reversedString);
+    }
+}
