009ea92a26dbbbbe8dc3e277ae48721c066bebed
Anubhav <anubahvagnihotri@gmail.com>
2023-11-15 14:27:39 +0530

    staircase program added.

---
+++ src/basicprograms/staircase/StairCase.java
@@ -0,0 +1,31 @@
+package basicprograms.staircase;
+
+import java.util.Scanner;
+    public class StairCase {
+        public static void main(String[] args) {
+            System.out.println("Enter the value of n");
+            Scanner sc= new Scanner(System.in);
+            int n = sc.nextInt();
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(i+j>=n-1){
+                        System.out.print("#");}
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+//Enter the value of n
+//5
+//    #
+//   ##
+//  ###
+// ####
+//#####
+//
+//This Pattern as output will be Printed.
