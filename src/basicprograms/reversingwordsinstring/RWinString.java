af2c4d9567e936e824fc05d3d5a213dafbc4f854
Anubhav <anubahvagnihotri@gmail.com>
2023-11-17 22:14:59 +0530

    reversing word in string program added.

---
+++ src/basicprograms/reversingwordsinstring/RWinString.java
@@ -0,0 +1,16 @@
+package basicprograms.reversingwordsinstring;
+
+import java.util.StringJoiner;
+
+public class RWinString {
+        public static void main(String[] args) {
+            String s = "   a good   example  ";
+            String[] c= s.trim().split("\\s+");
+            StringJoiner result= new StringJoiner(" ");
+            for(int i=c.length-1;i>=0;i--)
+            {
+                result.add(c[i]);
+            }
+            System.out.println(result.toString());
+        }
+}
