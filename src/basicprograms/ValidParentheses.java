dfb648edc33ecbb2c6f265bb3b08d23da4ffec36
Anubhav <anubahvagnihotri@gmail.com>
2023-11-21 14:14:03 +0530

    ValidParentheses code Added

---
+++ src/basicprograms/ValidParentheses.java
@@ -0,0 +1,25 @@
+package basicprograms;
+import java.util.Stack;
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "(})[]{";
+        char[] c=s.toCharArray();
+//      System.out.println(Arrays.toString(c));
+        Stack<Character> st = new Stack<>();
+
+            for(Character x:c)
+            {
+                if( (!st.isEmpty()) && (  (st.peek()=='(' && x==')') || (st.peek()=='{' && x=='}') || (st.peek()=='[' && x==']')  )  )
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(x);
+                }
+            }
+            System.out.println(st.isEmpty());
+    }
+}
+
+
