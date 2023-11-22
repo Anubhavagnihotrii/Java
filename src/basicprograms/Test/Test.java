5f4ba48c251c8e5afb2594d7b7c2339ced808d90
Anubhav <anubahvagnihotri@gmail.com>
2023-11-22 18:03:02 +0530

    hello world added

--- src/basicprograms/Test/Test.java
+++ src/basicprograms/Test/Test.java
@@ -1,2 +1,7 @@
-package basicprograms.Test;public class Test {
+package basicprograms.Test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
