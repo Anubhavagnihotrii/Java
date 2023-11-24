8e8bbc8f1f87b46c9ca10abb0cf3cb520199047a
Anubhav <anubahvagnihotri@gmail.com>
2023-11-24 20:53:25 +0530

    Adding the code for Rotating Image

--- src/basicprograms/rotateimage/Rotate.java
+++ src/basicprograms/rotateimage/Rotate.java
@@ -1,2 +1,28 @@
-package basicprograms.rotateimage;public class Rotate {
+package basicprograms.rotateimage;
+
+
+public class Rotate {
+    public static void main(String[] args) {
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+
+        int[][] newMatrix = new int[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix.length-1,x=0; j >=0  ; j--,x++) {
+                newMatrix[i][x]=matrix[j][i];
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length ; j++) {
+                matrix[i][j]=newMatrix[i][j];
+            }
+        }
+
+        for (int i = 0; i <matrix.length ; i++) {
+            for (int j = 0; j < matrix.length ; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
