f83ed9bc4458dc268451dc38b5e96cb070517b6a
Anubhav <anubahvagnihotri@gmail.com>
2023-12-01 21:58:53 +0530

    DiagonalDiffernce code added.

--- src/basicprograms/diagonaldifference/DiagonalDifference.java
+++ src/basicprograms/diagonaldifference/DiagonalDifference.java
@@ -1,2 +1,26 @@
-package basicprograms.diagonaldifference;public class DiagonalDifference {
+package basicprograms.diagonaldifference;
+
+public class DiagonalDifference {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6,},{9,8,9}};
+
+        int sum1=0;
+        int sum2 =0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length ; j++) {
+                if(i==j){
+                    sum1+=arr[i][j];
+                }}
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length ; j++) {
+                if(i+j== arr.length-1){
+                    sum2+=arr[i][j];
+                }}
+        }
+        System.out.println(Math.abs(sum1-sum2));
+
+    }
 }
+
