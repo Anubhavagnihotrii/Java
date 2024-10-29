77a830a686fcd97de2994060153d03a905213ad9
Anubhav <anubahvagnihotri@gmail.com>
2024-10-29 17:28:55 +0530

    updating selection sort code

--- src/sorting/selectionsort/SelectionSort.java
+++ src/sorting/selectionsort/SelectionSort.java
@@ -3,23 +3,25 @@ package sorting.selectionsort;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        int[] arr ={99,4,63,2,1,3,0,1,2};
-
-        System.out.println("Before Sorting "+ Arrays.toString(arr));
-
-        for(int i=0;i< arr.length;i++)
-        {
-            for(int j=i+1;j<arr.length;j++)
-            {
-                if(arr[i]<arr[j])
-                {
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+    public  static void selectionSort(int[] arr){
+        int  n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex=i;
+            for (int j = i+1; j < n; j++) {
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
                 }
             }
+            if(minIndex!=i){
+                int temp= arr[minIndex];
+                arr[minIndex]=arr[i];
+                arr[i]=temp;
+            }
         }
-        System.out.println("After Sorting "+ Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        int[] arr={2,4,677,24,788,32,5};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
