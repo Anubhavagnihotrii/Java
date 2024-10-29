3d43e2ee85f2227d9bb82bf4da68d40d5b17c9ef
Anubhav <anubahvagnihotri@gmail.com>
2024-10-29 22:48:02 +0530

    updating count sort

--- src/sorting/countsort/CountSort.java
+++ src/sorting/countsort/CountSort.java
@@ -4,42 +4,37 @@ import java.util.Arrays;
 
 //Count Sort is Non-Comparison Algo
 //Used for single digit whole numbers
-
+//Time Complexity O(n+k) -> Linear
 
 public class CountSort {
+
+    public  static void countSort(int[]arr){
+        int max=arr[0];
+        for (int i:arr){
+            max= Math.max(max,i);
+        }
+        System.out.println(max);
+        int[] cumCount = new int[max+1];
+        // Frequency of each number
+        for(int i:arr){
+            cumCount[i]++;
+        }
+        System.out.println(Arrays.toString(cumCount));
+        // cumulative count [current = current+previous]
+        for(int i=1;i< cumCount.length;i++){
+            cumCount[i]+=cumCount[i-1];
+        }
+        System.out.println(Arrays.toString(cumCount));
+
+        int[] output= new int[arr.length];
+
+        for (int i = arr.length-1; i >=0; i--) {
+            output[--cumCount[arr[i]]] = arr[i];
+        }
+        System.out.println(Arrays.toString(cumCount));
+    }
     public static void main(String[] args) {
         int[] arr = {3,5,6,3,3,2,1,5,9};
-        int max =arr[0];
-
-        //Finding the maximum value
-        for(int i:arr){
-            if(i>max){
-                max=i;
-            }
-        }
-//      System.out.println(max);
-
-        int[]cumulativeCount=new int[max+1];
-
-//      Putting frequency of each element to calculate cumulative count
-        for(int i:arr){
-            cumulativeCount[i]++;
-        }
-//        System.out.println(Arrays.toString(cumulativeCount));
-//      calculating cumulative count
-        for(int i=1;i<cumulativeCount.length;i++){
-            cumulativeCount[i]+=cumulativeCount[i-1];
-        }
-//        System.out.println(Arrays.toString(cumulativeCount));
-
-//      sorting array
-        int[] sortedArray = new  int[arr.length];
-        for(int i=arr.length-1;i>=0;i--){
-            int val = arr[i];
-            int pos = cumulativeCount[val]-1;
-            sortedArray[pos]=val;
-            cumulativeCount[val]=cumulativeCount[val]-1;
-        }
-        System.out.println(Arrays.toString(sortedArray));
+        countSort(arr);
     }
 }
