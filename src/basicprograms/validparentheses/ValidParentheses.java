a6fb697c0a0bb1272281817bf2ae0f2349e2b438
Anubhav <anubahvagnihotri@gmail.com>
2025-05-25 13:04:48 +0530

    organizing folder struc

--- src/basicprograms/validparentheses/ValidParentheses.java
+++ src/basicprograms/validparentheses/ValidParentheses.java
@@ -2,7 +2,8 @@ package basicprograms.validparentheses;
 import java.util.Stack;
 public class ValidParentheses {
     public static void main(String[] args) {
-        String s = "(})[]{";
+        // String s = "(})[]{";        //// Example of invalid parentheses
+        String s = "({[]})";           //// Example of valid parentheses
         char[] c=s.toCharArray();
 //      System.out.println(Arrays.toString(c));
         Stack<Character> st = new Stack<>();
